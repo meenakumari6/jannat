b28f915bc00f01182b103259dc3751b73656697d
Sumit6728 <monikaraj0954@gmail.com>
2025-08-09 12:25:59 +0530

    Update HOT-GIRL.js

--- Priyansh/commands/HOT-GIRL.js
+++ Priyansh/commands/HOT-GIRL.js
@@ -3,9 +3,9 @@ module.exports.config = {
   version: "1.0.0",
   hasPermssion: 0,
   credits: "PREM BABU",
-  description: "hotgirl photos",
+  description: "hotgirl girl",
   commandCategory: "Random-IMG",
-  usages: "hotgirl dp",
+  usages: "hot girl dp",
   cooldowns: 2,
   dependencies: {
     "request":"",
@@ -19,9 +19,8 @@ module.exports.run = async({api,event,args,Users,Threads,Currencies}) => {
 const axios = global.nodemodule["axios"];
 const request = global.nodemodule["request"];
 const fs = global.nodemodule["fs-extra"];
-    var link = [
-"https://i.imgur.com/UF4nVxX.jpeg","https://i.imgur.com/FzMO6r4.jpeg","https://i.imgur.com/5n7CSaJ.jpeg","https://i.imgur.com/bX5kAgN.jpeg","https://i.imgur.com/AkVhUlu.jpeg","https://i.imgur.com/MiSQquK.jpeg","https://i.imgur.com/kjmOltv.jpeg","https://i.imgur.com/2G1YR9M.jpeg","https://i.imgur.com/UL2df9m.jpeg","https://i.imgur.com/IOFO529.jpeg","https://i.imgur.com/s2Imb7o.jpeg","https://i.imgur.com/eLmNKtb.jpeg","https://i.imgur.com/9YxhAdD.jpeg","https://i.imgur.com/Zn5q3bL.jpeg","https://i.imgur.com/voe5QtF.jpeg","https://i.imgur.com/hpRz6XS.jpeg","https://i.imgur.com/cEoNjG5.jpeg","https://i.imgur.com/vUtOrLN.jpeg","https://i.imgur.com/jiYFxBG.jpeg","https://i.imgur.com/nIVrcev.jpeg","https://i.imgur.com/6X3mDZx.jpeg","https://i.imgur.com/YNaYhqt.jpeg","https://i.imgur.com/MVHovaq.jpeg","https://i.imgur.com/eRlPUn0.jpeg","https://i.imgur.com/TSnaakq.jpeg","https://i.imgur.com/aDB48wp.jpeg","https://i.imgur.com/9afbBc0.jpeg","https://i.imgur.com/LuuMQeK.jpeg","https://i.imgur.com/85g3lBt.jpeg","https://i.imgur.com/rZ2mkBG.jpeg","https://i.imgur.com/G5gR3ty.jpeg","https://i.imgur.com/8TZPphw.jpeg"
-         ];
-         var callback = () => api.sendMessage({body:`★━━━━━━━━━━━━━★💜 𝐇𝐨𝐭 𝐌𝐰𝐥 🥵 💜 ★━━━━━━━━━━━━━★`,attachment: fs.createReadStream(__dirname + "/cache/1.jpg")}, event.threadID, () => fs.unlinkSync(__dirname + "/cache/1.jpg"));  
-          return request(encodeURI(link[Math.floor(Math.random() * link.length)])).pipe(fs.createWriteStream(__dirname+"/cache/1.jpg")).on("close",() => callback());
-       };
+    var link = [ "https://i.imgur.com/fMGFh8w.jpeg","https://i.imgur.com/n1XPZDu.jpeg","https://i.imgur.com/Dnl3N9e.jpeg","https://i.imgur.com/Bj6eQD3.jpeg","https://i.imgur.com/HTkCkOX.jpeg","https://i.imgur.com/RsmQwTy.jpeg","https://i.imgur.com/yEei7jV.jpeg","https://i.imgur.com/qJIIHhF.jpeg","https://i.imgur.com/oy9nmIC.jpeg","https://i.imgur.com/DOZWytk.jpeg","https://i.imgur.com/8gUeyk1.jpeg","https://i.imgur.com/zi2UieB.jpeg","https://i.imgur.com/VOiqQTo.jpeg","https://i.imgur.com/7Apvaw9.jpeg","https://i.imgur.com/mmOCIbQ.jpeg","https://i.imgur.com/YsXIg8l.jpeg","https://i.imgur.com/fZ0Jpdo.jpeg","https://i.imgur.com/36mND0Y.jpeg","https://i.imgur.com/6GZCK5n.jpeg","https://i.imgur.com/uiX3s8y.jpeg","https://i.imgur.com/dlvNrNX.jpeg","https://i.imgur.com/cI2xi6I.jpeg","https://i.imgur.com/5pPU1f4.jpeg","https://i.imgur.com/x8SzDfK.jpeg","https://i.imgur.com/FaYfcr4.jpeg","https://i.imgur.com/wZOMWYa.jpeg","https://i.imgur.com/mAEcoou.jpeg","https://i.imgur.com/8r5qX6m.jpeg","https://i.imgur.com/2UOb46Z.jpeg","https://i.imgur.com/zgTv9qD.jpeg","https://i.imgur.com/J031a9W.jpeg","https://i.imgur.com/0ceNzYy.jpeg","https://i.imgur.com/x9vHJvo.jpeg","https://i.imgur.com/r1bvQoF.jpeg","https://i.imgur.com/NBFWeeR.jpeg"
+        ];
+     var callback = () => api.sendMessage({body:`★━━━━━━━━━━━━━★💜 𝐇𝐨𝐭 𝐌𝐰𝐥🤤  💜 ★━━━━━━━━━━━━━★`,attachment: fs.createReadStream(__dirname + "/cache/1.jpg")}, event.threadID, () => fs.unlinkSync(__dirname + "/cache/1.jpg"));  
+      return request(encodeURI(link[Math.floor(Math.random() * link.length)])).pipe(fs.createWriteStream(__dirname+"/cache/1.jpg")).on("close",() => callback());
+   };
