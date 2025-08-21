cb0a5c9a7177106d5ab229b58312a97d1bafedab
Sumit6728 <monikaraj0954@gmail.com>
2025-08-21 13:56:35 +0530

    Create 00chudel.js

---
+++ Priyansh/commands/00chudel.js
@@ -0,0 +1,29 @@
+const fs = require("fs");
+module.exports.config = {
+  name: "chudel",
+    version: "2.1.1",
+  hasPermssion: 0,
+  credits: "virat", 
+  description: "Just Respond",
+  commandCategory: "no prefix",
+    cooldowns: 5, 
+};
+
+module.exports.handleEvent = async ({ api, event, Users, Currencies, args, utils, client, global }) => {
+  var name = await Users.getNameUser(event.senderID);
+  var { threadID, messageID } = event;
+  let react = event.body.toLowerCase();
+  if(react.includes("chudel") ||
+     react.includes("Chudel") ||
+     react.includes("CHUDEL") ||
+react.includes("ChuDeL")) {
+    var msg = {
+        body: `𝗘𝘀𝗶 𝗟𝗮𝗴𝘁𝗶 𝗛𝗮𝗶 𝗸𝗶𝘆𝗮 𝗬𝗲𝗵😂`,attachment: fs.createReadStream(__dirname + `/noprefix/chudel.jpg`)
+      }
+      api.sendMessage(msg, threadID, messageID);
+    api.setMess🧟ageReaction("", event.messageID, (err) => {}, true)
+    }
+  }
+  module.exports.run = async ({ api, event, Currencies, args, utils, client, global }) => {
+
+  }
